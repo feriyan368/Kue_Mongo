f2f34bd2abdc569ca3e59c1b712858c25dc7351b
Feriyananggara <80140307+feriyan368@users.noreply.github.com>
2022-07-05 15:10:35 +0700

    Update kue.js

--- router/kue.js
+++ router/kue.js
@@ -7,6 +7,6 @@ routerKue.route("/Kue").get(controllerKue.getKue).post(controllerKue.insert);
 
 routerKue.route("/Kue/:kode").put(controllerKue.update).get(controllerKue.getKueByKode).delete(controllerKue.delete);
 
-routerKue.route("/Kue/Nama/:kode").get(controllerKue.getNamaByKode).put(controllerKue.insertNama);
+routerKue.route("/Kue/Nama/:kode").get(controllerKue.getKueByKode).put(controllerKue.insert);
 
 module.exports = routerKue;
